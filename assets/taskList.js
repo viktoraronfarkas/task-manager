10f40c514fdd40b1661760b1d4395ed73384c02c
Viktor Farkas <fv1ktor09@gmail.com>
2022-06-12 20:56:32 +0200

    Auto-generated commit [ci skip]

---
+++ assets/taskList.js
@@ -0,0 +1,42 @@
+export const taskList = [
+  {
+    id: 1,
+    title: 'Get groceries',
+    description: 'Milk, Eggs, Bread, Salt',
+    priority: 'medium',
+    dueDate: '',
+    resolvedAt: '',
+  },
+  {
+    id: 2,
+    title: 'Send important emails',
+    description: 'Send email to tax office, include reference number: 372983.',
+    priority: 'high',
+    dueDate: '02.06.2022',
+    resolvedAt: '',
+  },
+  {
+    id: 3,
+    title: 'Take Randy the dog to the vet',
+    description: 'Price of visit: 30 Euros. Withdraw cash.',
+    priority: 'medium',
+    dueDate: '12.04.2022',
+    resolvedAt: '11.04.2022',
+  },
+  {
+    id: 4,
+    title: 'Water Jane\'s plants',
+    description: 'Let the soil get fully soaked with water.',
+    priority: 'high',
+    dueDate: '29.05.2022',
+    resolvedAt: '',
+  },
+  {
+    id: 5,
+    title: 'Do the craftworks coding challenge',
+    description: 'Details in email.',
+    priority: 'high',
+    dueDate: '29.05.2022',
+    resolvedAt: '',
+  },
+];
